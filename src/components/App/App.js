33f4f60e9e608782d0604afe5672a57883231a31
vadivai <vaikanvn@gmail.com>
2023-12-12 23:51:08 +0200

    Finish

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Wrapper } from 'components/ContactsList/ContactsList.styled';
 import { ContactsList } from 'components/ContactsList/ContactsList';
@@ -6,37 +6,30 @@ import { FormAddContact } from 'components/FormAddContact/FormAddContact';
 import { Section } from './App.styled';
 import { Filter } from 'components/Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const getInitialContacts = () => {
+  const savedContacts = window.localStorage.getItem('contacts');
 
-  componentDidMount() {
-    const savedContacts = window.localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
   }
+  return []; // можно null?
+  // return savedContacts !== null ? JSON.parse(savedContacts) : [];
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      window.localStorage.setItem(
-        'contacts',
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
 
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const filteredItems = contacts.filter(item => {
+    const hasName = item.name.toLowerCase().includes(filter.toLowerCase());
+    return hasName;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
     const isExistContact = contacts.find(contact => {
       return contact.name.toLowerCase().includes(newContact.name.toLowerCase());
     });
@@ -45,50 +38,39 @@ export class App extends Component {
       return alert(`${newContact.name} is already in contacts`);
     }
 
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
-      };
-    });
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { ...newContact, id: nanoid() },
+    ]);
   };
 
-  filterContacts = newFilter => {
-    this.setState({ filter: newFilter });
+  const filterContacts = newFilter => {
+    setFilter(newFilter);
   };
 
-  deleteContact = idContact => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(item => item.id !== idContact),
-      };
-    });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredItems = contacts.filter(item => {
-      const hasName = item.name.toLowerCase().includes(filter.toLowerCase());
-      return hasName;
-    });
-
-    return (
-      <>
-        <Section>
-          <h2>Phonebook</h2>
-          <FormAddContact onAddContact={this.addContact} />
-        </Section>
-        <Section>
-          <h3>Contacts</h3>
-          <Filter filter={this.state.filter} onFilter={this.filterContacts} />
-          <Wrapper>
-            {filteredItems.length > 0 && (
-              <ContactsList items={filteredItems} onDelete={this.deleteContact}>
-                ContactsList
-              </ContactsList>
-            )}
-          </Wrapper>
-        </Section>
-      </>
+  const deleteContact = idContact => {
+    setContacts(prevContacts =>
+      prevContacts.filter(item => item.id !== idContact)
     );
-  }
-}
+  };
+
+  return (
+    <>
+      <Section>
+        <h2>Phonebook</h2>
+        <FormAddContact onAddContact={addContact} />
+      </Section>
+      <Section>
+        <h3>Contacts</h3>
+        <Filter filter={filter} onFilter={filterContacts} />
+        <Wrapper>
+          {filteredItems.length > 0 && (
+            <ContactsList items={filteredItems} onDelete={deleteContact}>
+              ContactsList
+            </ContactsList>
+          )}
+        </Wrapper>
+      </Section>
+    </>
+  );
+};
